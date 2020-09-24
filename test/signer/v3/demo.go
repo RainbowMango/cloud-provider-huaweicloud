e5f29f5f6bd63f3b4fddc8d3b02b3e43e0459628
RainbowMango <renhongcai@huawei.com>
2020-09-24 20:37:55 +0800

    Get signer v3 API on board.

---
+++ test/signer/v3/demo.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+
+	"sigs.k8s.io/cloud-provider-huaweicloud/test/signer/v3/legacysdk/core"
+)
+
+func main() {
+	demoApp()
+}
+
+func demoApp() {
+	// Create the HTTP request
+	req, err := http.NewRequest("GET", "http://vpc.cn-north-4.myhuaweicloud.com:443/v2.0/lbaas/loadbalancers?vip_address=10.38.173.101", ioutil.NopCloser(bytes.NewBuffer([]byte(""))))
+	if err != nil {
+		return
+	}
+	req.Close = true
+
+	// add the sign to request header if needed.
+	sign := core.Signer{
+		AccessKey: "cTNDcpJDkkBM8ZP72pwd",
+		SecretKey: "qpvqfcp4FcmCehcQdS1fExcXo03bGyvbaf5ugE9c",
+		Region:    "cn-north-4",
+		Service:   "ec2",
+	}
+	req.Header.Add("X-Project-Id", "88eecb5e3034473c817407e590eb3bca")
+	req.Header.Add("x-sdk-date", "20190829T122203Z")
+	if err := sign.Sign(req); err != nil {
+		return
+	}
+
+}
